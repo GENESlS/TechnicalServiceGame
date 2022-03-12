6ecc95676fd1f52abc2d6cee743b2d213a0bd3fc
GENESlS <81309902+GENESlS@users.noreply.github.com>
2022-03-13 00:21:50 +0300

    update

--- Assets/Scripts/sit.cs
+++ Assets/Scripts/sit.cs
@@ -8,18 +8,17 @@ public class sit : MonoBehaviour
     void Start()
     {
         temp = new Vector3(c.transform.localPosition.x, c.transform.localPosition.y, c.transform.localPosition.z);
-        c.transform.localPosition = new Vector3(0, 2, 2);
-        c.transform.Rotate(0, 171, 0);
         StartCoroutine(Startup());
         this.enabled = false;
     }
-
+    
     IEnumerator Startup()
     {
-        int z = 1;
-        yield return new WaitForSecondsRealtime(2);
+        c.transform.localPosition = new Vector3((float)-0.0350000001,(float)1.34800005, (float)0.690999985);
+        c.transform.Rotate(0, 180, 0);
+        yield return new WaitForSecondsRealtime(3);
         c.transform.localPosition = temp;
-        c.transform.Rotate(0, -171, 0);
+        c.transform.Rotate(0, -180, 0);
         c.SetActive(false);
         e.SetActive(true);
     }
