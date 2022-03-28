2d56665918f6da07b27e3b8fde721c8e257e9848
GENESlS <81309902+GENESlS@users.noreply.github.com>
2022-03-28 17:56:52 +0300

    asd

---
+++ Assets/CheckCoding.cs
@@ -0,0 +1,13 @@
+using TMPro;
+using UnityEngine;
+
+public class CheckCoding : MonoBehaviour
+{
+    public GameObject a, b, c, d, e, f, g;
+
+    public bool Check()
+    {
+
+        return true;
+    }
+}
