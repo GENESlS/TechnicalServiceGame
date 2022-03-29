17187d30581e298f3a068d92151f5b328121e2f9
GENESlS <81309902+GENESlS@users.noreply.github.com>
2022-03-29 18:39:53 +0300

    asd

--- Assets/Scripts/CheckEp2.cs
+++ Assets/Scripts/CheckEp2.cs
@@ -1,15 +1,35 @@
 using UnityEngine.UI;
+using System.Collections;
 using UnityEngine;
 
 public class CheckEp2 : MonoBehaviour
 {
-    public GameObject a,b;
+    public GameObject a, b, d, e, f;
     public Sprite c;
+    GameObject x;
+    AudioManager y;
+
+    private void Start()
+    {
+        x = GameObject.Find("AudioManager");
+        y = x.GetComponent<AudioManager>();
+    }
     public void Checker()
     {
         if (a.GetComponent<CheckCoding>().Check())
         {
             b.GetComponent<Image>().sprite = c;
+            StartCoroutine(Asd());
         }
+    }
+
+    private IEnumerator Asd()
+    {
+        e.SetActive(false);
+        f.SetActive(false);
+        yield return new WaitForSecondsRealtime(3);
+        d.SetActive(true);
+        y.StopPlaying("Theme");
+        y.Play("Success");
     }
 }
