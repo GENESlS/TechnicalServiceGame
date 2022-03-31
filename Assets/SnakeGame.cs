2e453c92f309cdca552150129104ec9ad1fbf4b7
GENESlS <81309902+GENESlS@users.noreply.github.com>
2022-03-31 19:02:41 +0300

    snek

--- Assets/SnakeGame.cs
+++ Assets/SnakeGame.cs
@@ -1,16 +1,69 @@
+using System.Collections;
 using UnityEngine;
 
 public class SnakeGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject x;
+    string pivot, keyPress;
+    public float speed = 2;
+    Vector3 movement;
+
+    private void Start()
     {
-        
+        pivot = "n";
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
-        
+        switch (keyPress)
+        {
+            case string key when key.Equals("w",System.StringComparison.CurrentCultureIgnoreCase) :
+                pivot = "n";
+                break;
+            case string key when key.Equals("a", System.StringComparison.CurrentCultureIgnoreCase):
+                pivot = "a";
+                break;
+            case string key when key.Equals("s", System.StringComparison.CurrentCultureIgnoreCase):
+                pivot = "s";
+                break;
+            case string key when key.Equals("d", System.StringComparison.CurrentCultureIgnoreCase):
+                pivot = "d";
+                break;
+            case "up":
+                pivot = "n";
+                break;
+            case "left":
+                pivot = "a";
+                break;
+            case "down":
+                pivot = "s";
+                break;
+            case "right":
+                pivot = "d";
+                break;
+        }
+
+        switch (pivot)
+        {
+            case "n":
+                movement = new Vector3(0, 5, 0);
+                x.transform.Translate(movement * speed * Time.deltaTime);
+                break;
+            case "a":
+                movement = new Vector3(-5, 0, 0);
+                x.transform.Translate(movement * speed * Time.deltaTime);
+                break;
+            case "s":
+                movement = new Vector3(0, -5, 0);
+                x.transform.Translate(movement * speed * Time.deltaTime);
+                break;
+            case "d":
+                movement = new Vector3(5, 0, 0);
+                x.transform.Translate(movement * speed * Time.deltaTime);
+                break;
+        }
+
+
     }
 }
