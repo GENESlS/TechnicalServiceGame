70761bd183fe2b40de16f408da0bf557ec9c6d9e
GENESlS <81309902+GENESlS@users.noreply.github.com>
2022-04-11 17:28:22 +0300

    update

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    private int sceneInd;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -12,5 +13,32 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("QUIT!");
         Application.Quit();
+    }
+
+    public void Loader(int a)
+    {
+        switch (a) 
+        {
+            case 1:
+                sceneInd = 1;
+                break;
+            case 2:
+                sceneInd = 3;
+                break;
+            case 3:
+                sceneInd = 5;
+                break;
+            case 4:
+                sceneInd = 7;
+                break;
+            case 5:
+                sceneInd = 9;
+                break;
+        }
+    }
+
+    public void Goo()
+    {
+        SceneManager.LoadScene(sceneInd);
     }
 }
