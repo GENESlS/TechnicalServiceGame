783b8a2ab9ceeb89124dbc6c48499f1d33afe40b
GENESlS <81309902+GENESlS@users.noreply.github.com>
2022-05-20 00:43:59 +0300

    asd

--- Assets/Scripts/AnimationDisable.cs
+++ Assets/Scripts/AnimationDisable.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 public class AnimationDisable : MonoBehaviour
 {
     public GameObject a, b;
+    private bool choice = false;
+    GameObject Manager;
+
+    private void Start()
+    {
+        Manager = GameObject.FindGameObjectWithTag("Manager");
+    }
     private void DisableCanvas()
     {
         a.SetActive(false);
@@ -13,5 +20,20 @@ public class AnimationDisable : MonoBehaviour
     {
         a.SetActive(true);
         b.SetActive(false);
+    }
+
+    private void AnimEndChoice()
+    {
+        a.SetActive(true);
+        b.SetActive(false);
+        if (choice)
+        {
+            Manager.GetComponent<HonorPoint>().Decrease(-25);
+        } else Manager.GetComponent<HonorPoint>().Decrease(25);
+    }
+
+    public void Answer(bool T)
+    {
+        choice = T;
     }
 }
