969f67891d98f4c7fa02f0b2946484d2b42de87d
GENESlS <81309902+GENESlS@users.noreply.github.com>
2022-05-29 19:00:26 +0300

    asd

---
+++ Assets/Scripts/CheatCodes.cs
@@ -0,0 +1,31 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheatCodes : MonoBehaviour
+{
+    public GameObject CheatCode, NoTimeButton;
+    private string CodeText;
+
+    public void Cheating()
+    {
+        CodeText = CheatCode.GetComponent<TMP_Text>().text;
+
+        if (CodeText == "259753")
+        {
+            SceneManager.LoadScene("Episode 2");
+        } else if (CodeText == "359753")
+        {
+            SceneManager.LoadScene("Episode 3");
+        } else if (CodeText == "459753")
+        {
+            SceneManager.LoadScene("Episode 4");
+        } else if (CodeText == "559753")
+        {
+            SceneManager.LoadScene("Episode 5");
+        } else if (CodeText == "KillTime")
+        {
+            NoTimeButton.SetActive(true);
+        }
+    }
+}
