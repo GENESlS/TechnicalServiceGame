74c7620deb3dea311e17927b9203fdc34b51bd18
GENESlS <81309902+GENESlS@users.noreply.github.com>
2022-05-30 00:08:27 +0300

    asd

--- Assets/Scripts/CheatCodes.cs
+++ Assets/Scripts/CheatCodes.cs
@@ -7,32 +7,48 @@ public class CheatCodes : MonoBehaviour
 {
     public static bool NoTimer = false;
     public GameObject CheatCode, NoTimeButton;
-    private string CodeText;
+    TMP_InputField tMP_InputField;
 
+    public AudioManager a;
+
+    private void Start()
+    {
+        tMP_InputField = CheatCode.GetComponent<TMP_InputField>();
+    }
     public void Cheating()
     {
-        CodeText = CheatCode.GetComponent<TMP_Text>().text;
-        Debug.Log(CodeText);
-        CheatCode.GetComponent<TMP_Text>().text = "";
-
-        switch (CodeText)
+        switch (tMP_InputField.text)
         {
             case "259753":
                 SceneManager.LoadScene("Episode 2");
+                NoTimer = false;
+                a.Play("Click");
+                //Debug.Log(CodeText);
                 break;
             case "359753":
                 SceneManager.LoadScene("Episode 3");
+                NoTimer = false;
+                a.Play("Click");
+                //Debug.Log(CodeText);
                 break;
             case "459753":
                 SceneManager.LoadScene("Episode 4");
+                NoTimer = false;
+                a.Play("Click");
+                //Debug.Log(CodeText);
                 break;
             case "559753":
                 SceneManager.LoadScene("Episode 5");
+                NoTimer = false;
+                a.Play("Click");
+                //Debug.Log(CodeText);
                 break;
             case "KillTime":
                 NoTimeButton.SetActive(true);
+                a.Play("Click");
+                //Debug.Log(CodeText);
                 break;
-        }
+        } 
        /* if (CodeText == "259753")
         {
             Debug.Log("aa");
